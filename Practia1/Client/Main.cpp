bc734f802c3222dc6f3c8331cd6f3023ff646973
Eloi Salles <oinu69@gmail.com>
2018-02-26 21:46:16 +0100

    Primer Commit. Contiene el Taller 2 (Servidor/Cliente TCP)
    Signed-off-by: Eloi Salles <oinu69@gmail.com>

---
+++ Practia1/Client/Main.cpp
@@ -0,0 +1,151 @@
+#include <SFML\Graphics.hpp>
+#include <SFML\Network.hpp>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+
+#define MAX_MENSAJES 30
+#define CLIENT_PORT 50000
+#define BUFFER_SIZE 2000
+
+
+using namespace std;
+std::mutex mu;
+
+//Escucha por un socket determinado y escribe el mensaje llegado.
+//socket: Es el socket al cual debemos escuchar.
+//recived: La longitud de los datos que recibiremos.
+//aMensajes: Es donde contendra todos los mensajes del chat.
+void RecivedFunction(sf::TcpSocket* socket,size_t* recived, vector<string>* aMensajes, sf::RenderWindow* window)
+{
+	while (window->isOpen())
+	{
+		char buffer[BUFFER_SIZE];
+		sf::Socket::Status status = socket->receive(buffer, sizeof(buffer), *recived);
+		string s = buffer;
+		if (status == sf::Socket::Status::Done)
+		{
+			mu.lock();
+			aMensajes->push_back(s);
+			if (aMensajes->size() > 25)
+			{
+				aMensajes->erase(aMensajes->begin(), aMensajes->begin() + 1);
+			}
+			mu.unlock();
+		}
+	}
+	
+}
+
+int main()
+{
+	//ESTABLECER CONNECION
+	sf::TcpSocket socket;
+	sf::TcpSocket::Status socketStatus;
+	size_t recived;
+
+	// Obtenemos nuestra direccion ip, y nos connectamos con el puerto indicado y nuestra ip
+	sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+	socket.connect(ip, CLIENT_PORT);
+
+	std::vector<std::string> aMensajes;
+
+	sf::Vector2i screenDimensions(800, 600);
+
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Chat");
+
+	sf::Font font;
+	if (!font.loadFromFile("Roboto-Bold.ttf"))
+	{
+		std::cout << "Can't load the font file" << std::endl;
+	}
+
+	sf::String mensaje = " >";
+
+	sf::Text chattingText(mensaje, font, 24);
+	chattingText.setFillColor(sf::Color(0, 160, 0));
+	chattingText.setStyle(sf::Text::Bold);
+
+
+	sf::Text text(mensaje, font, 24);
+	text.setFillColor(sf::Color(0, 160, 0));
+	text.setStyle(sf::Text::Bold);
+	text.setPosition(0, 560);
+
+	sf::RectangleShape separator(sf::Vector2f(800, 5));
+	separator.setFillColor(sf::Color(200, 200, 200, 255));
+	separator.setPosition(0, 550);
+
+	string msn;
+	
+	//RECIVE
+	//Se genera un thread (hilo), que escucha si llegan mensajes o no.
+	thread t(RecivedFunction, &socket, &recived, &aMensajes, &window);
+
+	while (window.isOpen())
+	{
+		sf::Event evento;
+		while (window.pollEvent(evento))
+		{
+			switch (evento.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (evento.key.code == sf::Keyboard::Escape)
+					window.close();
+				else if (evento.key.code == sf::Keyboard::Return)
+				{
+					// SEND
+					// Pasamos el mensaje a std::string para hacerlo mas facil en el momento de enviarlo.
+					msn = mensaje;
+					socketStatus =socket.send(msn.c_str(), msn.size() + 1);
+					if (socketStatus == sf::TcpSocket::Status::Disconnected)
+					{
+						msn = "Server Disconnected";
+						aMensajes.push_back(msn);
+						if (aMensajes.size() > 25)
+						{
+							aMensajes.erase(aMensajes.begin(), aMensajes.begin() + 1);
+						}
+					}
+
+					mensaje = ">";
+				}
+				break;
+			case sf::Event::TextEntered:
+				if (evento.text.unicode >= 32 && evento.text.unicode <= 126)
+					mensaje += (char)evento.text.unicode;
+				else if (evento.text.unicode == 8 && mensaje.getSize() > 0)
+					mensaje.erase(mensaje.getSize() - 1, mensaje.getSize());
+				break;
+			}
+		}
+		
+
+		window.draw(separator);
+		for (size_t i = 0; i < aMensajes.size(); i++)
+		{
+			std::string chatting = aMensajes[i];
+			chattingText.setPosition(sf::Vector2f(0, 20 * i));
+			chattingText.setString(chatting);
+			window.draw(chattingText);
+		}
+		std::string mensaje_ = mensaje + "_";
+		text.setString(mensaje_);
+		window.draw(text);
+
+
+		window.display();
+		window.clear();
+	}
+	
+
+	socket.disconnect();
+	t.join();
+
+}
---
+++ Practia1/Server/main.cpp
@@ -0,0 +1,150 @@
+#include <SFML\Graphics.hpp>
+#include <SFML\Network.hpp>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+
+#define MAX_MENSAJES 30
+#define SERVER_PORT 50000
+#define BUFFER_SIZE 2000
+
+
+using namespace std;
+
+void DisconnectUser(vector<sf::TcpSocket*>& list, sf::SocketSelector *ss, int i)
+{
+
+	//Lo eliminamos del Socket Selector
+	ss->remove(*list[i]);
+
+	//Desconectamos el socket del cliente
+	list[i]->disconnect();
+
+	delete(list[i]);
+
+	//Eliminamos el socket de la lista
+	list.erase(list.begin() + i, list.begin() + i + 1);
+
+	
+	//Informamos que un usuario se ha desconectado
+	for (sf::TcpSocket* socket : list)
+	{
+		string outMsn = "A User Disconected";
+		socket->send(outMsn.c_str(), outMsn.size() + 1);
+	}
+
+	cout << "User Disconnected" << endl;
+}
+
+int main()
+{
+	//ESTABLECER CONNECION
+	sf::SocketSelector ss;
+	vector<sf::TcpSocket*> socketList;
+	sf::TcpSocket::Status socketStatus;
+
+	sf::TcpListener listener;
+	sf::TcpListener::Status listenerStatus;
+
+	char buffer[BUFFER_SIZE];
+	string msn;
+	size_t recived;	
+
+	//Le indicamos a que puerto debe escuchar para el servidor
+	listenerStatus =listener.listen(SERVER_PORT);
+
+	//Añadimos el listener al Socket Selector
+	ss.add(listener);
+
+	while (listenerStatus != sf::TcpListener::Status::Disconnected)
+	{
+		//Mientras haya elementos en el Socket Selector, esperara a que algun socket le envie algo.
+		while (ss.wait())
+		{
+			//Comprovamos si es un Listener
+			if (ss.isReady(listener))
+			{
+
+				//Se añade el socket al Socket Selector
+				sf::TcpSocket* socket = new sf::TcpSocket();
+				//Esperamos peticion de un cliente
+				sf::TcpListener::Status st = listener.accept(*socket);
+				if (st == sf::TcpListener::Status::Done)
+				{
+					socketList.push_back(socket);
+				}
+				ss.add(*socket);
+
+				//Les indicamos que se ha connectado un nuevo usuario
+				msn = "New User Connected!";
+				for (sf::TcpSocket* s : socketList)
+				{
+					if(s!=socket)s->send(msn.c_str(), msn.size() + 1);
+				}
+
+				cout << "New User Connecte" << endl;
+			}
+
+			//Si no es el listener, sera un socket
+			else
+			{
+
+				//Miramos de que socket recivimos el mensaje
+				for (int j = 0; j < socketList.size(); j++)
+				{
+					//Encontramos el socket
+					if (ss.isReady(*socketList[j]))
+					{
+						//Recivimos el mensaje.
+						socketStatus = socketList[j]->receive(buffer, sizeof(buffer), recived);
+
+						//Pasar el contenido del buffer, al string de mensaje.
+						if (socketStatus == sf::TcpSocket::Status::Done)
+						{
+							msn = buffer;
+
+							//Enviamos el mensajes
+							for (int i = 0; i < socketList.size(); i++)
+							{
+								sf::TcpSocket::Status st = socketList[i]->send(msn.c_str(), msn.size() + 1);
+
+								if ( st == sf::TcpSocket::Status::Error)
+								{
+									cout << "Error al enviar" << endl;
+								}
+								
+							}
+						}
+
+						//Si se ha desconnectado
+						else if (socketStatus == sf::TcpSocket::Status::Disconnected)
+						{
+							DisconnectUser(socketList, &ss, j);
+						}
+
+						break;
+					}
+				}
+			}
+		}
+	}
+
+
+	//Vaciar el Socket Selector
+	ss.clear();
+
+	//Paramos de escuchar el puerto, para así dejarlo libre.
+	listener.close();
+
+	//Desconnectamos todos los clientes
+	for (sf::TcpSocket* &socket : socketList)
+	{
+		socket->disconnect();
+	}
+
+	//Limpiamos el vector
+	socketList.clear();
+
+}
